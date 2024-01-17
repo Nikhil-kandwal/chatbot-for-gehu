dcc853821aa5206985e948a8f2ab438972757c0e
Nikhil Kandwal <128395127+Nikhil-kandwal@users.noreply.github.com>
2024-01-17 19:19:33 +0530

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,9 +1,8 @@
-
 import random
 import json
 import torch
 from model import NeuralNet
-from nltk_use import bag_of_words, tokenize
+from nltk_utils import bag_of_words, tokenize
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
